9b44646b375beda1bf29d32696ab3b208e7c695e
Composta Puerta <adkadkwkq12@gmail.com>
2023-12-14 22:13:29 -0500

    upload

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\SeriesController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/series', [SeriesController::class, 'index']);
+Route::get('/series/criar', [SeriesController::class, 'create']);
+
+Route::post('/series/salvar', [SeriesController::class, 'store']);
